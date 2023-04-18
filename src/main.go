4875f8d4be88e7092ff3e3f927126f418d2ea546
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2023-04-19 03:20:27 +0900

    modify main.go

--- src/main.go
+++ src/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
   "go_sample/config"
+  "go_sample/database"
   "go_sample/router"
 )
 
 func main() {
   // configの初期化
   config.Init()
+
+  // DBの初期化
+  database.Init()
+  defer database.Close()
 
   // routerの初期化
   router.Init()
